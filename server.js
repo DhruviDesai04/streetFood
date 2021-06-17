3913737737890c22c767d637fdaf92ae30f4de6a
Dhruvi Desai <dhruvidesai4402@gmail.com>
2021-06-17 14:48:06 +0530

    auth system

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo');
+const passport = require('passport');
 
 // Database connection
 mongoose.connect(process.env.MONGO_CONNECTION_URL, {
@@ -46,15 +47,24 @@ app.use(
 );
 
 
+  // Passport config
+  const passportInit = require('./app/config/passport');
+  passportInit(passport);
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+
 app.use(flash());
 
 // Assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session;
+    res.locals.user = req.user;
     next();
 })
 
