110414b7fb78dca5982d31c2a5908b49861faff0
Dhruvi Desai <dhruvidesai4402@gmail.com>
2021-06-25 13:43:46 +0530

    socket integration

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const path = require('path');
-const expressLayout = require('express-ejs-layouts');
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3300;
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('express-flash');
-const MongoDbStore = require('connect-mongo');
-const passport = require('passport');
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
+const Emitter = require("events");
 
 // Database connection
 mongoose.connect(process.env.MONGO_CONNECTION_URL, {
@@ -33,50 +34,67 @@ connection
 //   collection: 'sessions'
 // })
 
+// Event Emmiter
+const eventEmitter = new Emitter();
+app.set("eventEmitter", eventEmitter);
+
 // Session config
 app.use(
   session({
     secret: process.env.COOKIES_SECRET,
     resave: false,
     store: MongoDbStore.create({
-      mongoUrl: process.env.MONGO_CONNECTION_URL
+      mongoUrl: process.env.MONGO_CONNECTION_URL,
     }),
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24 }, //24 hours
   })
 );
 
-
-  // Passport config
-  const passportInit = require('./app/config/passport');
-  passportInit(passport);
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
 // Assets
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Global middleware
 app.use((req, res, next) => {
-    res.locals.session = req.session;
-    res.locals.user = req.user;
-    next();
-})
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next();
+});
 
 // set Template engine
-app.use(expressLayout)
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
-require('./routes/web')(app);
+require("./routes/web")(app);
 
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
 
+// Socket
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  // Join
+  socket.on("join", (roomName) => {
+    socket.join(roomName);
+  });
+});
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+eventEmitter.on("orderUpdated", (data) => {
+  io.to(`order_${data.id}`).emit("orderUpdated", data);
+});
+
+eventEmitter.on("orderPlaced", (data) => {
+  io.to("adminRoom").emit("orderPlaced", data);
 });
